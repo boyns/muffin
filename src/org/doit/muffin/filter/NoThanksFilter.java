7bde49c3c040764cd67d7f926ae48d300e7c617c
boyns <boyns>
2000-01-25 21:19:33 +0000

    * also filter application/x-javascript

--- src/org/doit/muffin/filter/NoThanksFilter.java
+++ src/org/doit/muffin/filter/NoThanksFilter.java
@@ -1,4 +1,4 @@
-/* $Id: NoThanksFilter.java,v 1.7 2000/01/25 06:14:52 boyns Exp $ */
+/* $Id: NoThanksFilter.java,v 1.8 2000/01/25 21:19:33 boyns Exp $ */
 
 /*
  * Copyright (C) 1996-2000 Mark R. Boyns <boyns@doit.org>
@@ -36,6 +36,7 @@ public class NoThanksFilter implements ContentFilter, RequestFilter, ReplyFilter
     InputObjectStream in = null;
     OutputObjectStream out = null;
     Request request = null;
+    Reply reply = null;
 
     public NoThanksFilter(NoThanks factory)
     {
@@ -85,8 +86,10 @@ public class NoThanksFilter implements ContentFilter, RequestFilter, ReplyFilter
     public boolean needsFiltration(Request request, Reply reply)
     {
 	this.request = request;
+	this.reply = reply;
 	String s = reply.getContentType();
-	return s != null && s.startsWith("text/html");
+	return s != null
+	    && (s.startsWith("text/html") || s.startsWith("application/x-javascript"));
     }
     
     public void setInputObjectStream(InputObjectStream in)
@@ -105,131 +108,13 @@ public class NoThanksFilter implements ContentFilter, RequestFilter, ReplyFilter
 
 	try
 	{
-	    String endTag = null;
-	    boolean killingComment = false;
-
-	    Object obj;
-            while ((obj = in.read()) != null)
-            {
-		Token token = (Token) obj;
-		if (killingComment && token.getType() != Token.TT_COMMENT)
-		{
-		    continue;
-		}
-
-		switch (token.getType())
-		{
-		case Token.TT_COMMENT:
-		    String s = token.toString();
-		    killingComment = factory.killComment(s);
-		    if (killingComment)
-		    {
-			factory.report(request, "removed comment " + s);
-		    }
-		    // AJP modification: Ignore everything up to endTag, if set
-		    else if (endTag == null)
-		    {
-			out.write(token);
-		    }
-		    break;
-
-		case Token.TT_SCRIPT:
-		    token = factory.processScript(request, token);
-		    if (token != null)
-		    {
-			out.write(token);
-		    }
-		    break;
-		    
-		case Token.TT_TAG:
-		    boolean output = true;
-		    Tag tag = token.createTag();
-
-		    // AJP modification: Ignore everything up to endTag, if set
-		    if (endTag != null)
-		    {
-			if (tag.is(endTag))
-			{
-			    endTag = null;
-			}
-			continue;
-		    }
-		    
-		    if (factory.stripTag(tag.name()))
-		    {
-			factory.report(request, "stripped " + tag.name());
-			endTag = factory.stripUntil(tag.name());
-			output = false;
-		    }
-		    if (output && factory.replaceTag(tag.name()))
-		    {
-			Tag newTag = factory.replaceTagWith(tag.name());
-			factory.report(request, "replaced " + tag.name()
-				       + " with " + newTag.name());
-			tag = newTag;
-		    }
-		    if (output && factory.checkTagAttributes(tag))
-		    {
-			if (factory.processTagAttributes(request, tag))
-			{
-			    output = false;
-			    if (factory.hasEnd(tag.name()))
-			    {
-				endTag = "/" + tag.name();
-			    }
-			}
-		    }
-		    if (output && factory.checkTag(tag.name()) && tag.attributeCount() > 0)
-		    {
-			Enumeration e = tag.enumerate();
-			while (e.hasMoreElements())
-			{
-			    String attr = (String) e.nextElement();
-			    if (factory.checkAttr(attr))
-			    {
-				String link = tag.get(attr);
-				if (link != null && factory.isKilled(link))
-				{
-				    /* Can't kill tags like body and head.  Instead
-				       remove the offending attribute. */
-				    if (factory.isRequired(tag.name()))
-				    {
-					factory.report(request, "removed "
-						       + attr + "=" + link);
-					tag.remove(attr);
-				    }
-				    /* Kill the tag completely. */
-				    else
-				    {
-					factory.report(request, "killed "
-						       + tag.name()
-						       + " with "
-						       + attr + "=" + link);
-					output = false;
-					if (factory.hasEnd(tag.name()))
-					{
-					    endTag = "/" + tag.name();
-					}
-					break; /* tag is killed */
-				    }
-				}
-			    }
-			}
-		    }
-		    if (output)
-		    {
-			token.importTag(tag);
-			out.write(token);
-		    }
-		    break;
-		    
-		default:
-		    if (endTag == null)
-		    {
-			out.write(token);
-		    }
-		    break;
-		}
+	    if ("application/x-javascript".equals(reply.getContentType()))
+	    {
+		filterJavascript();
+	    }
+	    else
+	    {
+		filterHtml();
 	    }
 	}
 	catch (IOException ioe)
@@ -245,6 +130,157 @@ public class NoThanksFilter implements ContentFilter, RequestFilter, ReplyFilter
 	    }
 	    catch (IOException ioe)
 	    {
+	    }
+	}
+    }
+    
+    private void filterJavascript()
+	throws IOException
+    {
+	Object obj;
+	Token script = new Token(Token.TT_SCRIPT);
+
+	// read the javascript code into one token
+	// so it can be filtered 
+	while ((obj = in.read()) != null)
+	{
+	    script.append(((ByteArray)obj).getBytes());
+	}
+
+	script = factory.processScript(request, script);
+	if (script != null)
+	{
+	    out.write(script);
+	}
+    }
+
+    private void filterHtml()
+	throws IOException
+    {
+	String endTag = null;
+	boolean killingComment = false;
+
+	Object obj;
+	while ((obj = in.read()) != null)
+	{
+	    Token token = (Token) obj;
+	    if (killingComment && token.getType() != Token.TT_COMMENT)
+	    {
+		continue;
+	    }
+
+	    switch (token.getType())
+	    {
+	    case Token.TT_COMMENT:
+		String s = token.toString();
+		killingComment = factory.killComment(s);
+		if (killingComment)
+		{
+		    factory.report(request, "removed comment " + s);
+		}
+		// AJP modification: Ignore everything up to endTag, if set
+		else if (endTag == null)
+		{
+		    out.write(token);
+		}
+		break;
+
+	    case Token.TT_SCRIPT:
+		token = factory.processScript(request, token);
+		if (token != null)
+		{
+		    out.write(token);
+		}
+		break;
+		    
+	    case Token.TT_TAG:
+		boolean output = true;
+		Tag tag = token.createTag();
+
+		// AJP modification: Ignore everything up to endTag, if set
+		if (endTag != null)
+		{
+		    if (tag.is(endTag))
+		    {
+			endTag = null;
+		    }
+		    continue;
+		}
+		    
+		if (factory.stripTag(tag.name()))
+		{
+		    factory.report(request, "stripped " + tag.name());
+		    endTag = factory.stripUntil(tag.name());
+		    output = false;
+		}
+		if (output && factory.replaceTag(tag.name()))
+		{
+		    Tag newTag = factory.replaceTagWith(tag.name());
+		    factory.report(request, "replaced " + tag.name()
+				   + " with " + newTag.name());
+		    tag = newTag;
+		}
+		if (output && factory.checkTagAttributes(tag))
+		{
+		    if (factory.processTagAttributes(request, tag))
+		    {
+			output = false;
+			if (factory.hasEnd(tag.name()))
+			{
+			    endTag = "/" + tag.name();
+			}
+		    }
+		}
+		if (output && factory.checkTag(tag.name()) && tag.attributeCount() > 0)
+		{
+		    Enumeration e = tag.enumerate();
+		    while (e.hasMoreElements())
+		    {
+			String attr = (String) e.nextElement();
+			if (factory.checkAttr(attr))
+			{
+			    String link = tag.get(attr);
+			    if (link != null && factory.isKilled(link))
+			    {
+				/* Can't kill tags like body and head.  Instead
+				   remove the offending attribute. */
+				if (factory.isRequired(tag.name()))
+				{
+				    factory.report(request, "removed "
+						   + attr + "=" + link);
+				    tag.remove(attr);
+				}
+				/* Kill the tag completely. */
+				else
+				{
+				    factory.report(request, "killed "
+						   + tag.name()
+						   + " with "
+						   + attr + "=" + link);
+				    output = false;
+				    if (factory.hasEnd(tag.name()))
+				    {
+					endTag = "/" + tag.name();
+				    }
+				    break; /* tag is killed */
+				}
+			    }
+			}
+		    }
+		}
+		if (output)
+		{
+		    token.importTag(tag);
+		    out.write(token);
+		}
+		break;
+		    
+	    default:
+		if (endTag == null)
+		{
+		    out.write(token);
+		}
+		break;
 	    }
 	}
     }
