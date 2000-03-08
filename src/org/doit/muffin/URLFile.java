550a651dab307b0a518710910dbd157b81ffcee2
boyns <boyns>
2000-03-08 15:20:03 +0000

    * new file implementation to easily represent local and remote files

---
+++ src/org/doit/muffin/URLFile.java
@@ -0,0 +1,54 @@
+/* $Id: URLFile.java,v 1.1 2000/03/08 15:20:03 boyns Exp $ */
+
+/*
+ * Copyright (C) 1996-2000 Mark R. Boyns <boyns@doit.org>
+ *
+ * This file is part of Muffin.
+ *
+ * Muffin is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Muffin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Muffin; see the file COPYING.  If not, write to the
+ * Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ */
+package org.doit.muffin;
+
+import java.io.*;
+import java.net.*;
+
+public class URLFile implements UserFile
+{
+    private URL url;
+
+    URLFile(URL url)
+    {
+	this.url = url;
+    }
+    
+    public String getName()
+    {
+	return url.toString();
+    }
+
+    public InputStream getInputStream()
+	throws IOException
+    {
+	// XXX this doesn't use MuffinResolver
+	return url.openStream();
+    }
+    
+    public OutputStream getOutputStream()
+	throws IOException
+    {
+	throw new IOException("Can't output to " + url);
+    }
+}
