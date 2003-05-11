31c3697195f7fbe9a6c296e9accd11d430920414
cmallwitz <cmallwitz>
2003-05-11 23:12:06 +0000

    Updated handling of status code 304

--- src/org/doit/muffin/Reply.java
+++ src/org/doit/muffin/Reply.java
@@ -34,16 +34,16 @@ import java.util.StringTokenizer;
 public class Reply extends Message
 {
     public static final String GzipContentLengthAttribute = "X-Gzip-Content-length";
-    
+
     private final int NoContentStatusCode = 204;
     private final int NotModifiedStatusCode = 304;
-    
+
     private InputStream in = null;
     private Request request = null;
     private int statusCode = -1;
 
     /** Construct an empty http/https reply.
-     */    
+     */
     public Reply()
     {
     }
@@ -56,41 +56,41 @@ public class Reply extends Message
      *
      * @see read
      * @param in Input stream.
-     */    
+     */
     public Reply(InputStream in)
     {
-	setContent(in);
+        setContent(in);
     }
 
     /** Set the request associated with this reply.
      * @param request Request
-     */    
+     */
     public void setRequest (Request request)
     {
-	this.request = request;
+        this.request = request;
     }
 
     /** Get the request associatd with this reply.
      * @return The request.
-     */    
+     */
     public Request getRequest ()
     {
-	return request;
+        return request;
     }
 
     /** Set the input stream which contains the content for this reply.
      * @param in Input stream.
-     */    
+     */
     public void setContent(InputStream in)
     {
-	this.in = in;
+        this.in = in;
     }
 
     /** Get the input stream containing the content for this reply.
      *
      * Any filter which reads this content must wrap this InputStream must reset the stream after reading.
      * For example:
-     * 
+     *
      * <CODE>
      *    in = new BufferedInputStream (reply.getContent ());
      *    in.mark (java.lang.Integer.MAX_VALUE);
@@ -99,25 +99,25 @@ public class Reply extends Message
      *    reply.setContent (in);
      * </CODE>
      * @return Content stream.
-     */    
+     */
     public InputStream getContent()
     {
-	return in;
+        return in;
     }
-    
+
     /** Read the status line and headers from the input stream supplied to the constructor.
      *
      * The setContent and getContent methods set and get this input stream.
      * @throws IOException Thrown if there are any errors reading the stream.
-     */    
+     */
     public void read() throws IOException
     {
-	if (in != null)
-	{
-	    read(in);
-	}
+        if (in != null)
+        {
+            read(in);
+        }
     }
-    
+
     /** Read the status line and headers from the input stream.
      *
      * The setContent and getContent methods set and get this input stream.
@@ -126,60 +126,62 @@ public class Reply extends Message
      *
      * @param in Input stream. Used in place of any stream supplied in the constructor or by calling the setContent method.
      * @throws IOException Thrown if there are any errors reading the stream.
-     */    
+     */
     public void read(InputStream in) throws IOException
     {
-	statusLine = readLine(in);
-	if (statusLine == null || statusLine.length() == 0)
-	{
-	    throw new IOException("Missing HTTP status line");
-	}
+        statusLine = readLine(in);
+        if (statusLine == null || statusLine.length() == 0)
+        {
+            throw new IOException("Missing HTTP status line");
+        }
 
-	/* Look for HTTP/0.9 */
-	if (!statusLine.startsWith("HTTP"))
-	{
-	    /* Put back the line */
-	    if (this.in != null)
-	    {
-		String putback = new String(statusLine + "\n");
-		this.in = new SequenceInputStream(new StringBufferInputStream(putback), in);
-	    }
-	    /* Fake a status line and upgrade to HTTP/1.0 */
-	    statusLine = "HTTP/1.0 200 OK";
-	    return;
-	}
-	
-	readHeaders(in);
-	int code = getStatusCode();
+        /* Look for HTTP/0.9 */
+        if (!statusLine.startsWith("HTTP"))
+        {
+            /* Put back the line */
+            if (this.in != null)
+            {
+                String putback = new String(statusLine + "\n");
+                this.in = new SequenceInputStream(new StringBufferInputStream(putback), in);
+            }
+            /* Fake a status line and upgrade to HTTP/1.0 */
+            statusLine = "HTTP/1.0 200 OK";
+            return;
+        }
 
-	/* RFC 2068: 204 and 304 MUST NOT contain a message body. */
-	switch (code)
-	{
+        readHeaders(in);
+        int code = getStatusCode();
+
+        /* RFC 2616: 204 and 304 MUST NOT contain a message body. */
+        switch (code)
+        {
         case NoContentStatusCode:
-	case NotModifiedStatusCode:
-	    /* Ignore the message body if it exists */
-	    if (containsHeaderField("Content-length"))
-	    {
-		System.out.println("RFC 2068: Ignoring message-body from " + code + " response - "
-				    + getHeaderField("Server"));
-		int contentLength = 0;
-		try 
-		{
-		    contentLength = Integer.parseInt(getHeaderField("Content-length"));
-		}
-		catch (NumberFormatException e)
-		{
-		}
-		int n;
-		byte buffer[] = new byte[8192];
-		while ((n = in.read(buffer, 0, buffer.length)) > 0)
-		{
-		    /* ignore */
-		}
-		removeHeaderField("Content-length");
-	    }
-	    break;
-	}
+        case NotModifiedStatusCode:
+            /* Ignore the message body if it exists */
+            if (containsHeaderField("Content-length") && !getHeaderField("Content-length").equals("0"))
+            {
+                System.out.println("RFC 2616: Ignoring message-body from " + code +
+                                   " response - length " + getHeaderField("Content-length") + " " +
+                                   getHeaderField("Server"));
+
+                int contentLength = 0;
+                try
+                {
+                    contentLength = Integer.parseInt(getHeaderField("Content-length"));
+                }
+                catch (NumberFormatException e)
+                {
+                }
+                int n;
+                byte buffer[] = new byte[8192];
+                while ((n = in.read(buffer, 0, buffer.length)) > 0)
+                {
+                    /* ignore */
+                }
+                removeHeaderField("Content-length");
+            }
+            break;
+        }
     }
 
     /** Returns whether the reply has any content.
@@ -187,124 +189,124 @@ public class Reply extends Message
      * Replies with status code 204 "No Content" and 304 "Not Modified" are considered to have no content.
      * @return Returns false if the status code is 204 or 304, true otherwise.
      *
-     */    
+     */
     public boolean hasContent()
     {
-	switch (getStatusCode())
-	{
+        switch (getStatusCode())
+        {
         case NoContentStatusCode:
-	case NotModifiedStatusCode:
-	    return false;
+        case NotModifiedStatusCode:
+            return false;
 
-	default:
-	    return true;
-	}
+        default:
+            return true;
+        }
     }
 
     /** Get the http protocol, i.e. "GET", "HEAD", "POST" or "PUT".
      * @return Protocol.
-     */    
+     */
     public String getProtocol()
     {
-	StringTokenizer st = new StringTokenizer(statusLine);
-	String protocol = (String) st.nextToken();
-	return protocol;
+        StringTokenizer st = new StringTokenizer(statusLine);
+        String protocol = (String) st.nextToken();
+        return protocol;
     }
 
     /** Get the http status code.
      * @return Status code.
-     */    
+     */
     public int getStatusCode()
     {
-	if (statusCode == -1)
-	{
-	    StringTokenizer st = new StringTokenizer(statusLine);
-	    String protocol = (String) st.nextToken();
-	    String status = (String) st.nextToken();
+        if (statusCode == -1)
+        {
+            StringTokenizer st = new StringTokenizer(statusLine);
+            String protocol = (String) st.nextToken();
+            String status = (String) st.nextToken();
 
-	    try
-	    {
-		statusCode = Integer.parseInt(status);
-	    }
-	    catch (NumberFormatException e)
-	    {
-		System.out.println("Malformed or missing status code");
-		statusCode = 0;
-	    }
-	}
-	
-	return statusCode;
+            try
+            {
+                statusCode = Integer.parseInt(status);
+            }
+            catch (NumberFormatException e)
+            {
+                System.out.println("Malformed or missing status code");
+                statusCode = 0;
+            }
+        }
+
+        return statusCode;
     }
 
     private Hashtable headerParser(String header)
     {
-	Hashtable table = new Hashtable();
-	String type = getHeaderField(header);
-	if (type == null)
-	{
-	    return table;
-	}
+        Hashtable table = new Hashtable();
+        String type = getHeaderField(header);
+        if (type == null)
+        {
+            return table;
+        }
 
-	StringTokenizer st = new StringTokenizer(type, ";");
-	int count = 0;
-	while (st.hasMoreTokens())
-	{
-	    String token = st.nextToken();
-	    token = token.trim();
-	    String name;
-	    String value;
-	    int i = token.indexOf('=');
-	    if (i != -1)
-	    {
-		name = token.substring(0, i);
-		value = token.substring(i+1);
-	    }
-	    else
-	    {
-		name = token;
-		value = "";
-	    }
+        StringTokenizer st = new StringTokenizer(type, ";");
+        int count = 0;
+        while (st.hasMoreTokens())
+        {
+            String token = st.nextToken();
+            token = token.trim();
+            String name;
+            String value;
+            int i = token.indexOf('=');
+            if (i != -1)
+            {
+                name = token.substring(0, i);
+                value = token.substring(i+1);
+            }
+            else
+            {
+                name = token;
+                value = "";
+            }
 
-	    if (count == 0)
-	    {
-		table.put(header, name);
-	    }
-	    else
-	    {
-		table.put(name, value);
-	    }
+            if (count == 0)
+            {
+                table.put(header, name);
+            }
+            else
+            {
+                table.put(name, value);
+            }
 
-	    count++;
-	}
+            count++;
+        }
 
-	return table;
+        return table;
     }
 
     /** Get the content type.
      * @return Content type.
-     */    
+     */
     public String getContentType()
     {
-	Hashtable table = headerParser("Content-type");
-	return(String) table.get("Content-type");
+        Hashtable table = headerParser("Content-type");
+        return(String) table.get("Content-type");
     }
 
     /** Get the boundary parameter from the Content-type header.
      * @return Boundary string.
-     */    
+     */
     public String getBoundary()
     {
-	Hashtable table = headerParser("Content-type");
-	return(String) table.get("boundary");
+        Hashtable table = headerParser("Content-type");
+        return(String) table.get("boundary");
     }
 
     /** Get the value of the Transfer-Encoding header field.
      * @return Transfer encoding
-     */    
+     */
     public String getTransferEncoding()
     {
-	Hashtable table = headerParser("Transfer-Encoding");
-	return(String) table.get("Transfer-Encoding");
+        Hashtable table = headerParser("Transfer-Encoding");
+        return(String) table.get("Transfer-Encoding");
     }
 
     /** Get the chunk size from the input stream.
@@ -313,21 +315,21 @@ public class Reply extends Message
      * @param in Input stream with the chunk size line.
      * @throws IOException Thrown if there are any errors reading the input stream.
      * @return Chunk size.
-     */    
+     */
     public int getChunkSize(InputStream in) throws IOException
     {
-	String line = readLine(in);
-	line = line.trim(); /* apache can have trailing spaces */
-	int size = -1;
-	try
-	{
-	    size = Integer.valueOf(line, 16).intValue();
-	}
-	catch (NumberFormatException e)
-	{
-	    System.out.println(e);
-	}
-	return size;
+        String line = readLine(in);
+        line = line.trim(); /* apache can have trailing spaces */
+        int size = -1;
+        try
+        {
+            size = Integer.valueOf(line, 16).intValue();
+        }
+        catch (NumberFormatException e)
+        {
+            System.out.println(e);
+        }
+        return size;
     }
 
     /** Read past the chunked footer.
@@ -335,49 +337,49 @@ public class Reply extends Message
      * Reads lines from the input stream until end of stream or a line containing a ':' has been read.
      * @param in Input stream
      * @throws IOException Thrown if there are any errors reading from the stream.
-     */    
+     */
     public void getChunkedFooter(InputStream in) throws IOException
     {
-	for (;;)
-	{
-	    String line = readLine(in);
-	    if (line == null)
-	    {
-		break;
-	    }
-	    int i = line.indexOf(':');
-	    if (i == -1)
-	    {
-		break;
-	    }
-	}
+        for (;;)
+        {
+            String line = readLine(in);
+            if (line == null)
+            {
+                break;
+            }
+            int i = line.indexOf(':');
+            if (i == -1)
+            {
+                break;
+            }
+        }
     }
 
     /** Get the http status line.
      * @return Status line.
-     */    
+     */
     public String getStatusLine()
     {
-	return statusLine;
+        return statusLine;
     }
 
     /** Set the http status line.
      * @param line Status line.
-     */    
+     */
     public void setStatusLine(String line)
     {
-	this.statusLine = line;
+        this.statusLine = line;
     }
 
     /** Create a reply which redirects to the url.
      * @param url Url to redirect to.
      * @return Redirect reply.
-     */    
+     */
     public static Reply createRedirect(String url)
     {
-	Reply r = new Reply();
-	r.setStatusLine("HTTP/1.0 302 Moved Temporarily");
-	r.setHeaderField("Location", url);
-	return r;
+        Reply r = new Reply();
+        r.setStatusLine("HTTP/1.0 302 Moved Temporarily");
+        r.setHeaderField("Location", url);
+        return r;
     }
 }
