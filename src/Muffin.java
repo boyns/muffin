f0fa8423b2d54eec34ae70bd220be6f7ff74eddd
boyns <boyns>
2003-06-04 18:36:37 +0000

    * new Muffin loader and jar packaging * muffin.jar is now similar to a war with WEB-INF/lib and WEB-INF/classes * lib jar entries are extracted to tmpdir * classes entries are loaded directly * added dnsjava and gnu-regexp jars to lib * removed sources for dnsjava and gnu-regexp * muffin.jar is now built from the build directory - see build.xml

--- src/Muffin.java
+++ src/Muffin.java
@@ -1,7 +1,5 @@
-/* $Id: Muffin.java,v 1.6 2000/01/24 04:01:59 boyns Exp $ */
-
 /*
- * Copyright (C) 1996-2000 Mark R. Boyns <boyns@doit.org>
+ * Copyright (C) 1996-2003 Mark R. Boyns <boyns@doit.org>
  *
  * This file is part of Muffin.
  *
@@ -21,6 +19,138 @@
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
  */
 
-public class Muffin extends org.doit.muffin.Main
+import java.net.*;
+import java.io.*;
+import java.util.*;
+import java.util.jar.*;
+import java.lang.reflect.*;
+
+public class Muffin
 {
+    private final String        TMP_PREFIX = "Muffin-";
+    private final String        TMP_SUFFIX = ".jar";
+    private URLClassLoader      classLoader;
+
+    Muffin(String[] args)
+        throws Exception
+    {
+        init();
+        doit(args);
+    }
+
+    private void init()
+        throws Exception
+    {
+        StringTokenizer st;
+        String          path = null;
+
+        cleanup();
+
+        st = new StringTokenizer(System.getProperty("java.class.path"),
+                                 System.getProperty("path.separator"));
+        while (st.hasMoreTokens())
+        {
+            String entry = st.nextToken();
+            if (entry.toLowerCase().endsWith("muffin.jar"))
+            {
+                path = entry;
+                break;
+            }
+        }
+
+        if (path != null)
+        {
+            JarFile     jar;
+            ArrayList   urls;
+            URL         context;
+
+            context = new URL("jar:file:" + path + "!/");
+
+            urls = new ArrayList();
+            urls.add(new URL(context, "WEB-INF/classes/"));
+
+            jar = new JarFile(path);
+            for (Enumeration e = jar.entries(); e.hasMoreElements(); )
+            {
+                JarEntry jentry = (JarEntry)e.nextElement();
+                String name = jentry.getName();
+
+                if (name.startsWith("WEB-INF/lib/") &&
+                    name.toLowerCase().endsWith(".jar"))
+                {
+                    File file = File.createTempFile(TMP_PREFIX, TMP_SUFFIX);
+                    extractJar(jar.getInputStream(jentry), file);
+                    file.deleteOnExit();
+                    urls.add(file.toURL());
+                }
+            }
+
+            classLoader = new URLClassLoader((URL[])urls.toArray(new URL[0]));
+        }
+    }
+
+    private void cleanup()
+        throws IOException
+    {
+        File    tmpdir;
+        File[]  files;
+
+        tmpdir = new File(System.getProperty("java.io.tmpdir", "/tmp"));
+        files = tmpdir.listFiles();
+
+        for (int i = 0; i < files.length; i++)
+        {
+            if (files[i].getName().startsWith(TMP_PREFIX) &&
+                files[i].getName().endsWith(TMP_SUFFIX))
+            {
+                files[i].delete();
+            }
+        }
+    }
+
+    private void extractJar(InputStream in, File to)
+        throws IOException
+    {
+        byte[]          buf;
+        int             n;
+        OutputStream    out = null;
+
+        try
+        {
+            out = new FileOutputStream(to);
+            buf = new byte[8192];
+            while ((n = in.read(buf)) != -1)
+            {
+                out.write(buf, 0, n);
+            }
+            in.close();
+        }
+        finally
+        {
+            if (out != null)
+            {
+                try
+                {
+                    out.close();
+                }
+                catch (IOException e)
+                {
+                }
+            }
+        }
+    }
+
+    private void doit(String args[])
+        throws Exception
+    {
+        Class clazz = classLoader.loadClass("org.doit.muffin.Main");
+        Method method = clazz.getMethod("main", new Class[]{args.getClass()});
+        method.invoke(null, new Object[]{args});
+    }
+
+    public static void main(String[] args)
+        throws Exception
+    {
+        new Muffin(args);
+    }
 }
